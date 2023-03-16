431ab3b4291ec94261c6a1e9b5b3e6de763855d7
kalema ronald <kalemar8@gmail.com>
2023-03-16 10:53:40 -0700

    corrwct put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,11 @@
 
 /**
  * display program
+ *
  * using puts function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	/**
-	 * value zero is returned
-	 */
 }
