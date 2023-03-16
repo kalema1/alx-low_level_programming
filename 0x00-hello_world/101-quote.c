05bc21d655ef453497143d0d948d4f8ecabd36d2
kalema ronald <kalemar8@gmail.com>
2023-03-16 14:44:31 -0700

    unix library

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing using unix library
+ *
+ * Return: value 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
