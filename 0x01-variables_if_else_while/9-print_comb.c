d9a3fc53ee08a3945352890760a95468348a2e1f
kalema ronald <kalemar8@gmail.com>
2023-03-17 09:50:59 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - displays combination if a single digit
+ *
+ * Return: vale 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
