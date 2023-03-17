961c13f0ad1c60eeaa9930297215c812534608c2
kalema ronald <kalemar8@gmail.com>
2023-03-17 07:28:48 -0700

    numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - displays base 10 values
+ *
+ * Return: value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
