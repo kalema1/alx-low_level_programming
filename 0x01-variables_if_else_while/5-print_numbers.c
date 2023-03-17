edb1e0c9b15da62376c31052df14f10ba76fde3f
kalema ronald <kalemar8@gmail.com>
2023-03-17 07:18:48 -0700

    base 10 values

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - display base 10 numbers
+ *
+ * Return: value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
