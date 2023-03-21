9ce3f1a22389cd2c4a498e73c17347c05571d88e
kalema ronald <kalemar8@gmail.com>
2023-03-21 10:36:26 -0700

    lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints lowercase alphabet
+*
+*Return: no return value
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
