80d2b0ac8a1a3948887c7369dc35f2e0efd37117
kalema ronald <kalemar8@gmail.com>
2023-03-22 10:04:24 -0700

    fib numberss

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,26 @@
 */
 int main(void)
 {
-	int i;
+	int i = 0;
+	long j = 1, k = 2;
 
-	for (i = 1; i <= 50; i++)
+	while (i < 50)
 	{
-		printf("%d", i);
-		if (i != 50)
+		if (i == 0)
 		{
-			printf(", ");
+			printf("%ld", j);
 		}
+		else if (i == 1)
+		{
+			printf(", %ld", k);
+		}
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		i++;
 	}
 	printf("\n");
 	return (0);
