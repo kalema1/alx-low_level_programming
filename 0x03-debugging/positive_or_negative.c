f0343ab5b6b1720fc59fc29401ede98db736dcad
kalema ronald <kalemar8@gmail.com>
2023-03-23 02:02:08 -0700

    test positvie 2

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* positive_or_negative - give positive or negative
+* @i: value to be tested
+*
+* Return: no value
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
