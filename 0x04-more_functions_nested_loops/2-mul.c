71a51f6c5f3cf4c9f6338076a5b0de19d898395e
kalema ronald <kalemar8@gmail.com>
2023-03-23 10:33:36 -0700

    mulple of 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - muple of 2 numbers
+* @a: digit 1
+* @b: digit 2
+*
+* Return: value product
+*/
+int mul(int a, int b)
+{
+	int pdt;
+
+	pdt = (a * b);
+	return (pdt);
+}
