a37b7457290273eb8a60856124d4e8b59e29cb8b
kalema ronald <kalemar8@gmail.com>
2023-03-23 11:08:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+* _putchar - write charater to stdout
+* @c: the character to print
+*
+* Return: on success 1
+* on error, -1 is returned, and errno is set appropratly
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
