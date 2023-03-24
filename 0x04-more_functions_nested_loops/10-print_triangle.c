468930b990752ea6b2f1ff8a929595c3c8fa8f1b
kalema ronald <kalemar8@gmail.com>
2023-03-23 23:31:21 -0700

    triangle display

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_triangle - display triangle
+* @size: size of the triangle
+*
+* Return: no value
+*/
+void print_triangle(int size)
+{
+	int row, space, symbol;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = 1; (space <= (size - row)); space++)
+		{
+			_putchar(' ');
+		}
+		for (symbol = 1; symbol <= row; symbol++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
