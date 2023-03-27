57d78c7088781764c37ccaf7ae2acdec5ecfebb3
kalema ronald <kalemar8@gmail.com>
2023-03-27 05:09:42 -0700

    puts 2 of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts2 - print character
+* @str: character string
+*
+* Return: no value
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
