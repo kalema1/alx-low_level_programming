ef490bdc486e13df828bed29b28629d169710ea4
kalema ronald <kalemar8@gmail.com>
2023-04-03 08:07:12 -0700

    chess board 1

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,13 +15,7 @@ void print_chessboard(char (*a)[8])
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			if ((i == 0 && j == 7) || (i == 2 && j == 7) ||
-			(i == 3 && j == 7) || (i == 4 && j == 7) ||
-			(i == 5 && j == 7) || (i == 6 && j == 7))
-			{
-				_putchar('\n');
-			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
