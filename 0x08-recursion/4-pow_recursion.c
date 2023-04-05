28636403025fe2e5fb39aa6d7d5148f17472efc9
kalema ronald <kalemar8@gmail.com>
2023-04-05 03:27:21 -0700

    powering a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _pow_recursion - power value cal
+* @x: number
+* @y: power value
+*
+* Return: power value
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y-1));
+}
