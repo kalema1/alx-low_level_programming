7080dc8d4a247ffd641d1d5fc0bc63a834b271a7
kalema ronald <kalemar8@gmail.com>
2023-04-11 10:54:44 -0700

    free space

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* free_grid - free dimension
+* @grid: grid
+* @height: height
+*
+* Return: no value
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
