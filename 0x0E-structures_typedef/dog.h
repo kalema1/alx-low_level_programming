4f94660fe177d9e2f907869605a6e37fb6ddfc2c
kalema ronald <kalemar8@gmail.com>
2023-04-17 10:21:38 -0700

    structure in c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - userdefined datatypt fo a dog
+* @name: name of dog
+* @age: dog age
+* @owner: dog owner
+*
+* Description: dog details cartegories
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
