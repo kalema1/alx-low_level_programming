f6620deb1124decaaa69d7fdd24c82c4e81c688b
kalema ronald <kalemar8@gmail.com>
2023-04-17 22:17:25 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* new_dog - create a new dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+* Return: value 0
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	dog_t *d;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->name = malloc(i * sizeof(d->name));
+	if (d->name == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		d->name[k] = name[k];
+	d->age = age;
+	d->owner = malloc(j * sizeof(d->owner));
+	if (d->owner == NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		d->owner[k] = owner[k];
+	return (d);
+}
