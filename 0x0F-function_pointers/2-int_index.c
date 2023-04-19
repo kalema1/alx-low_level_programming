3733488902ea6e77a10ed43b18dcc1fbb1991380
kalema ronald <kalemar8@gmail.com>
2023-04-19 15:14:01 -0700

    search is over

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]) != 0)
-				return (i);
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
 		}
 	}
 	return (-1);
