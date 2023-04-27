acee69f25900e3edbd1774305aebb1b63991c148
kalema ronald <kalemar8@gmail.com>
2023-04-27 04:36:51 -0700

    hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,12 @@
-	global	main
 	extern	printf
+
+	SECTION .data
+fmt: db "Hello, Holberton", 10, 0
+	SECTION .text
+	global	main
 main:
-	mov	edi, format
+	mov	edi, fmt
 	xor	eax, eax
 	call	printf
 	mov	eax, 0
 	ret
-format: db 'Hello, Holberton', 0
