5626f203de7c4b8f4a56dcb2b2302de95665d0bb
kalema ronald <kalemar8@gmail.com>
2023-05-08 03:53:22 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* create_file - creates a file
+* @filename: file name
+* @text_content: pointer to the file
+*
+* Return: value 1 or -1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l]; l++)
+			;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, l);
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
